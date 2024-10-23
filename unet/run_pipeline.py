b63585cc275ca54cb95555b9fe577315e7355238
kelsdoerksen <kelseydoerksen@gmail.com>
2024-10-23 11:57:25 +0100

    Fixing district masks

--- unet/run_pipeline.py
+++ unet/run_pipeline.py
@@ -15,6 +15,8 @@ from utils import *
 from model import models, unet_modules
 from dataset import *
 import logging
+from osgeo import gdal
+import
 
 
 def get_args():
@@ -39,6 +41,42 @@ def get_args():
                         required=True)
 
     return parser.parse_args()
+
+
+def generate_district_masks(file_name):
+    '''
+    Create the masks for each district from Nepal raster
+    '''
+    # Load in Nepal District file
+    ds = gdal.Open('{}'.format(file_name))
+    array = ds.GetRasterBand(1).ReadAsArray()
+
+    # Create dict to match pixel values
+    district_dict = {'Bhojpur': 1, 'Dhankuta': 2, 'Ilam': 3, 'Jhapa': 4, 'Khotang': 5, 'Morang': 6, 'Okhaldhunga': 7,
+    'Panchthar': 8, 'Sankhuwasabha': 9, 'Solukhumbu': 10, 'Sunsari': 11, 'Taplejung': 12, 'Terhathum': 13,
+    'Udayapur': 14, 'Bara': 15, 'Dhanusha': 16, 'Mahottari': 17, 'Parsa': 18, 'Rautahat': 19, 'Saptari': 20,
+    'Sarlahi': 21, 'Siraha': 22, 'Bhaktapur': 23, 'Chitawan': 24, 'Dhading': 25, 'Dolakha': 26,
+    'Kabhrepalanchok': 27, 'Kathmandu': 28, 'Lalitpur': 29, 'Makawanpur': 30, 'Nuwakot': 31, 'Ramechhap': 32,
+    'Rasuwa': 33, 'Sindhuli': 34, 'Sindhupalchok': 35, 'Baglung': 36, 'Gorkha': 37, 'Kaski': 38, 'Lamjung': 39,
+    'Manang': 40, 'Mustang': 41, 'Myagdi': 42, 'Nawalparasi_W': 43, 'Parbat': 44, 'Syangja': 45, 'Tanahu': 46,
+    'Arghakhanchi': 47, 'Banke': 48, 'Bardiya': 49, 'Dang': 50, 'Gulmi': 51, 'Kapilbastu': 52, 'Palpa': 53,
+    'Nawalparasi_E': 54, 'Pyuthan': 55, 'Rolpa': 56, 'Rukum_E': 57, 'Rupandehi': 58, 'Dailekh': 59, 'Dolpa': 60,
+    'Humla': 61, 'Jajarkot': 62, 'Jumla': 63, 'Kalikot': 64, 'Mugu': 65, 'Rukum_W': 66, 'Salyan': 67,
+    'Surkhet': 68, 'Achham': 78, 'Baitadi': 70, 'Bajhang': 71, 'Bajura': 72, 'Dadeldhura': 73, 'Darchula': 74,
+    'Doti': 75, 'Kailali': 76, 'Kanchanpur': 77}
+
+    new_dict = {}
+    for k, v in district_dict.items():
+        # Only include District of interest
+        new_array = array.copy()
+        new_array[new_array != v] = 0
+        # Bound between 0 and 1
+        filtered_array = new_array.copy()
+        # Landslide class set to 1
+        filtered_array[filtered_array == v] = 1
+        new_dict[k] = filtered_array
+
+    return new_dict
 
 
 if __name__ == '__main__':
@@ -85,7 +123,7 @@ if __name__ == '__main__':
 
     # --- Generating District Masks for Custom Metrics calculations
     # Generating district masks to use for the precision, recall
-    district_masks = generate_district_masks('{}/Nepal_Rasterized_GPMRes_PerDistrict.tif'.format(root_dir))
+    district_masks = generate_district_masks('{}/District_Labels.tif'.format(root_dir))
 
     # We can locate now here where all the ones in the district are
     for district in district_masks:
