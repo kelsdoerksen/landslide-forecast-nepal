fd966caece2583e6a35aadfe9600cb038a304e13
kelsdoerksen <kelseydoerksen@gmail.com>
2024-10-23 12:16:30 +0100

    Fixing data loader

--- unet/dataset.py
+++ unet/dataset.py
@@ -6,6 +6,7 @@ from torch.utils.data import Dataset
 from torchvision import transforms
 from numpy import load, sort
 import os
+import torch
 
 class LandslideDataset(Dataset):
     def __init__(self, image_dir, label_dir, split):
@@ -29,9 +30,6 @@ class LandslideDataset(Dataset):
             image_fns = [x for x in image_fns if "2023" in x]
             label_fns = [x for x in label_fns if "2023" in x]
 
-        import ipdb
-        ipdb.set_trace()
-        
         image_fn = image_fns[index]
         label_fn = label_fns[index]
         image_fp = os.path.join(self.image_dir, image_fn)
@@ -39,6 +37,7 @@ class LandslideDataset(Dataset):
         multichannel_image = load('{}'.format(image_fp), allow_pickle=True).astype('float32')
         label_class = load('{}'.format(label_fp), allow_pickle=True)
         multichannel_image = self.transform(multichannel_image)
+        multichannel_image = torch.transpose(multichannel_image, 0, 1)
         label_class = self.transform(label_class)
 
         return multichannel_image.float(), label_class.float()
