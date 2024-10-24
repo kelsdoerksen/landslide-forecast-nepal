Fixing bcel oss, removing threshold and precision for now

unet/predict.py
@@ -23,14 +23,14 @@ def predict(in_model, test_dataset, wandb_experiment, seed, out_dir, device, dis
     threshold = 0.2
 
     # Setting model to eval mode
-    unet = models.UNet(n_channels=32, n_classes=1)
-    unet.load_state_dict(torch.load(in_model)['state_dict'])
-    unet.eval()
+    unetmodel = models.UNet(n_channels=32, n_classes=1)
+    unetmodel.load_state_dict(torch.load(in_model)['state_dict'])
+    unetmodel.eval()
 
     # Data loader for test set
     test_loader = DataLoader(test_dataset, batch_size=10, shuffle=True)
 
-    loss_criterion = nn.BCELoss()
+    loss_criterion = nn.BCEWithLogitsLoss()
     bce_score = 0
     precision = 0
     recall = 0
@@ -43,24 +43,23 @@ def predict(in_model, test_dataset, wandb_experiment, seed, out_dir, device, dis
             inputs, labels = inputs.to(device), labels.to(device)
 
             # predict the mask
-            outputs = unet(inputs)
+            outputs = unetmodel(inputs)
+
+            # Apply sigmoid for predictions
+            outputs_probs = torch.sigmoid(outputs)
 
             # Append first to preserve image shape for future plotting
             gt.append(labels.detach().numpy())
-            preds.append(outputs.detach().numpy())
+            preds.append(outputs_probs.detach().numpy())
 
             bce_score += loss_criterion(outputs, labels)
-            precision += precision_threshold(labels, inputs, threshold, district_masks)
-            recall += recall_threshold(labels, inputs, threshold, district_masks)
+            #precision += precision_threshold(labels, inputs, threshold, district_masks)
+            #recall += recall_threshold(labels, inputs, threshold, district_masks)
 
     print('test set BCE is: {}'.format(bce_score / len(test_loader)))
-    print('test set Precision is: {}'.format(precision / len(test_loader)))
-    print('test set Recall is: {}'.format(recall / len(test_loader)))
 
     wandb_experiment.log({
         'test set BCE': bce_score / len(test_loader),
-        'test set Precision': precision / len(test_loader),
-        'test set Recall': recall / len(test_loader)
     })
 
     for i in range(len(gt)):

unet/train.py
@@ -86,8 +86,8 @@ def train_model(model,
             # Apply sigmoid for probabilities for precision recall
             outputs_probs = torch.sigmoid(outputs)
 
-            thr_precision = precision_threshold(labels, outputs_probs, threshold, district_masks)
-            thr_recall = precision_threshold(labels, outputs_probs, threshold, district_masks)
+            #thr_precision = precision_threshold(labels, outputs_probs, threshold, district_masks)
+            #thr_recall = precision_threshold(labels, outputs_probs, threshold, district_masks)
 
             grad_scaler.scale(loss).backward()      # Compute partial derivative of the output f with respect to each of the input variables
             grad_scaler.step(optimizer)             # Updates value of parameters according to strategy
@@ -95,13 +95,11 @@ def train_model(model,
 
             global_step += 1
             epoch_loss += loss.item()
-            epoch_thr_precision += thr_precision
-            epoch_thr_recall += thr_recall
+            #epoch_thr_precision += thr_precision
+            #epoch_thr_recall += thr_recall
 
         experiment.log({
             'train BCE loss': epoch_loss/len(train_loader),
-            'train Precision': epoch_thr_precision/len(train_loader),
-            'train Recall': epoch_thr_recall/len(train_loader),
             'step': global_step,
             'epoch': epoch,
             'optimizer': opt
@@ -134,24 +132,22 @@ def train_model(model,
                 # Apply sigmoid for probabilities
                 voutputs_probs = torch.sigmoid(voutputs)
 
-                v_prec = precision_threshold(vlabels, voutputs_probs, threshold, district_masks)
-                v_rec = recall_threshold(labels, voutputs_probs, threshold, district_masks)
+                #v_prec = precision_threshold(vlabels, voutputs_probs, threshold, district_masks)
+                #v_rec = recall_threshold(labels, voutputs_probs, threshold, district_masks)
 
                 running_vloss += vloss
-                running_thr_recall += v_rec
-                running_thr_precision += v_prec
+                #running_thr_recall += v_rec
+                #running_thr_precision += v_prec
 
         avg_vloss = running_vloss / len(val_loader)
-        avg_prec = running_thr_precision / len(val_loader)
-        avg_rec = running_thr_precision / len(val_loader)
+        #avg_prec = running_thr_precision / len(val_loader)
+        #avg_rec = running_thr_precision / len(val_loader)
 
         logging.info('Validation BCE score: {}'.format(avg_vloss))
         try:
             experiment.log({
                 'learning rate': optimizer.param_groups[0]['lr'],
                 'validation BCE loss': avg_vloss,
-                'validation Precision': avg_prec,
-                'validation Recall': avg_rec,
                 'step': global_step,
                 'epoch': epoch,
                 **histograms