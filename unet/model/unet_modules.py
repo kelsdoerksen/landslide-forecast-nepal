e47d606ae2d02188ce9eccaf0600669487ad2469
kelsdoerksen <kelseydoerksen@gmail.com>
2025-06-22 18:46:12 +0100

    Fixing dropout

--- unet/model/unet_modules.py
+++ unet/model/unet_modules.py
@@ -22,10 +22,10 @@ class DoubleConv(nn.Module):
 
         self.double_conv = nn.Sequential(
             nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding=1, bias=False),
-            nn.Dropout(p=dropout),
+            nn.Dropout(p=float(dropout)),
             nn.ReLU(inplace=True),
             nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=1, bias=False),
-            nn.Dropout(p=dropout),
+            nn.Dropout(p=float(dropout)),
             nn.ReLU(inplace=True)
         )
 
